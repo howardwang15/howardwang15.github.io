85e9114cd4003067684b11276003cc251f28f5bc
howardwang15 <joy_luck_2000@hotmail.com>
2019-09-11 10:16:38 -0700

    added changes to master

--- src/actions/page.js
+++
@@ -1,6 +0,0 @@
-export const SWITCH_PAGE = 'SWITCH_PAGE';
-export const HOME = 'HOME';
-export const ABOUT_ME = 'ABOUT_ME';
-export const PORTFOLIO = 'PORTFOLIO';
-export const CONTACT = 'CONTACT';
-export const RESUME = 'RESUME';
