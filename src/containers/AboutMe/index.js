99ae1fdb1c935412a23c80aa25da7eb4ae55b5e2
howardwang15 <joy_luck_2000@hotmail.com>
2019-12-22 15:14:30 -0800

    updated about me

--- src/containers/AboutMe/index.js
+++ src/containers/AboutMe/index.js
@@ -6,10 +6,11 @@ export default class AboutMe extends React.Component {
         return (
             <div className='container'>
                 <div className='about-me'>
-                    I'm a passionate, fun-loving second-year student studying CS at the UCLA. I've been a coder for about 4 years now, specializing in machine learning/data science
-                    and full-stack web development. My favorite programming language is by far Python because of its flexibility and ease of use.
-                    My goal in life is to use different software technologies to build products in order to make life easier for everyone. In my spare time, 
-                    I enjoy expressing my creativity through various projects that run across my mind, analyzing data, playing and watching basketball, and hanging out with
+                    I'm a passionate, fun-loving third-year student studying CS at the UCLA. I've been a coder for about 5 years now, specializing in machine learning/data science
+                    and full-stack web development.
+                    My goal in life is to use different software technologies to build products in order to make life easier for everyone. In the past, I've
+                    interned at Qualcomm, Vertebrae, and Honey (recently acquired by PayPal).
+                    In my spare time, I enjoy expressing my creativity through various projects that run across my mind, analyzing data, playing and watching basketball, and hanging out with
                     friends and family.
                 </div>
                 <div className='image-container' />
